059586d23ca03c956d5743ccbd5ada84918ab6bd
AlvinTan <hqtan1988@gmail.com>
2017-09-26 15:22:12 +0800

    update

--- NhejProcess.py
+++ NhejProcess.py
@@ -1,10 +1,40 @@
-import numpy as np 
-import os 
+import numpy as np
+import os
+import NhejState
 
-class NhejPtr: 
-	def __init__(self,data): 
-		self.totalTime = 4 * 3600 # in seconds 
-		self.numDSB    = len(dat[:,0])
+
+class NhejPtr:
+	def __init__(self,data):
+		self.totalTime = 4 * 3600 # in seconds
+		self.currTime  = 0
+		self.numDSB    = len(data[:,0])
 		self.D1        = 170*100 # in angstrom^2/s
-		self.D2        = self.D1/10 
-	
+		self.D2        = self.D1/10
+		self.data = data
+		self.stateList = [] # contain state objects
+
+		# Initialize all dsb states
+		self._initializeDsbStates()
+
+		# Run each iterations using Gillespie algorithm
+		if self.currTime<self.totalTime:
+			self.oneIteration()
+			# log data!
+
+	def _initializeDsbStates(self):
+		for k in range(data[:,0]):
+			if dsbMasterData[k,3] == 1:
+				stateList.append(NhejState.ComplexUnjoinedDsbState())
+			else:
+				stateList.append(NhejState.SimpleDsbState())
+
+	def oneIteration(self):
+		dt = self._timeStepping()
+		
+
+	def _timeStepping(self):
+		dt_List = [self.stateList[k].getDeltaTime() for k in self.numDSB]
+		return min(dt_List)
+
+	def positionStepping(self):
+		pass
--- NhejState.py
+++ NhejState.py
@@ -3,12 +3,14 @@ import os
 
 class SimpleDsbState:
     def __init__(self):
+        self.rateConstant = [1,1,1] #in per second
         self.simpleDSB = 0
         self.ku_Li     = 0
         self.synapse   = 0
         self.null      = 0
+        self._initialize()
 
-    def initalize(self):
+    def _initalize(self):
         self.simpleDSB += 1
 
     def getStateAsVector(self):
@@ -28,6 +30,88 @@ class SimpleDsbState:
 
     def stateCheck(self):
         stateVector = self.getStateAsVector()
-        if any(stateVector>1):
+        if any(stateVector>1) or sum(stateVector)>1:
             raise ValueError("Invalid values for state")
-    
+
+    def stateStepping(self,dt):
+        pass
+
+    def getDeltaTime(self):
+        self.stateCheck()
+        stateVector = self.getStateAsVector()
+        ind = stateVector.index(1)
+        r = np.random.uniform()
+        return 1/self.rateConstant[ind] * np.log(1/r)
+
+class ComplexUnjoinedDsbState:
+    def __init__(self):
+        self.rateConstant = [1,1]
+        self.complexDSB = 0
+        self.ku        = 0
+        self.PKcs      = 0
+        self._initialize()
+
+    def _initialize(self):
+        self.complexDSB += 1
+
+    def getStateAsVector(self):
+        return [self.complexDSB,self.ku,self.PKcs]
+
+    def stateChange1(self):
+        self.complexDSB -= 1
+        self.ku += 1
+
+    def stateChange2(self):
+        self.ku -= 1
+        self.PKcs += 1
+
+    def stateCheck(self):
+        stateVector = self.getStateAsVector()
+        if any(stateVector>1) or sum(stateVector)>1:
+            raise ValueError("Invalid values for state")
+
+    def getDeltaTime(self):
+        self.stateCheck()
+        stateVector = self.getStateAsVector()
+        ind = stateVector.index(1)
+        r = np.random.uniform()
+        return 1/self.rateConstant[ind] * np.log(1/r)
+
+class ComplexJoinedDsbState:
+    def __init__(self):
+        self.rateConstant = [1,1,1]
+        self.synapse = 0
+        self.repairing = 0
+        self.Li_XR = 0
+        self.null = 0
+        self._initialize()
+
+    def _initialize(self):
+        self.synapse += 1
+
+    def getStateAsVector(self):
+        return [self.synapse,self.reparing,self.Li_XR,self.null]
+
+    def stateChange1(self):
+        self.synapse -= 1
+        self.repairing += 1
+
+    def stateChange2(self):
+        self.repairing -= 1
+        self.Li_XR += 1
+
+    def stateChange3(self):
+        self.Li_XR -= 1
+        self.null += 1
+
+    def stateCheck(self):
+        stateVector = self.getStateAsVector()
+        if any(stateVector>1) or sum(stateVector)>1:
+            raise ValueError("Invalid values for state")
+
+    def getDeltaTime(self):
+        self.stateCheck()
+        stateVector = self.getStateAsVector()
+        ind = stateVector.index(1)
+        r = np.random.uniform()
+        return 1/self.rateConstant[ind] * np.log(1/r)
