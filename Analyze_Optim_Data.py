c67ffffe58ce899d7911ef18a29018cf081b74ab
AlvinTan <hqtan1988@gmail.com>
2018-02-14 15:56:57 +0800

    update

---
+++ Analyze_Optim_Data.py
@@ -0,0 +1,28 @@
+import numpy as np
+from ExperimentData import Ce_137_gamma
+import matplotlib.pyplot as plt
+
+class AnalyzeData:
+    def __init__(self):
+        self.dirName ='Nhej_Repair_Outfiles/XLConstant'
+        self.numFiles = 3
+        self._PlotSimulationData()
+        self._PlotData()
+
+    def _PlotSimulationData(self):
+        for k in range(self.numFiles):
+            fname = self.dirName + str(k) + '.txt'
+            data = np.loadtxt(fname)
+            plt.plot(data[0,:],data[1,:],c=np.random.rand(3,1),label = str(k))
+
+    def _PlotData(self):
+        data = Ce_137_gamma().GetData_40Gy()
+        plt.plot(data[:,0]*60,data[:,1]/30.,'ro',label='Ce-137')
+        plt.xlabel('Time/mins')
+        plt.xlim(0,75)
+        plt.ylabel('Probability')
+        plt.legend()
+        plt.show()
+
+if __name__ == '__main__':
+    AnalyzeData()
--- Optimizer_Engine.py
+++ Optimizer_Engine.py
@@ -7,12 +7,15 @@ from NhejMath import Calculate_spatial_prob
 
 class OptimizerWrapper:
 	def __init__(self,*argTuple):
-		self.numTrials = 3
-		self.ParamList = np.linspace(10000,20000,self.numTrials)
+		self.numTrials = 5
+		#self.ParamList = np.linspace(10000,20000,self.numTrials)
+		self.ParamList = np.linspace(0.02,1.0,5)/60
 
 		for k in range(self.numTrials):
-			fname = 'Nhej_Repair_Outfiles/DiffusionConstant' + str(k) + '.txt'
-			self._ChangeDiffusionConstant(k)
+			fname = 'Nhej_Repair_Outfiles/XLConstant' + str(k) + '.txt'
+			#self._ChangeDiffusionConstant(k)
+			self._ChangeRateConstSimple(k)
+			self._ChangeRateConstComplex(k)
 			nhejPtr = NhejProcess(*argTuple)
 			nhejPtr.SaveOptimData(fname)
 
@@ -23,9 +26,9 @@ class OptimizerWrapper:
 		Calculate_spatial_prob.delta = self.ParamList[k]
 
 	def _ChangeRateConstSimple(self,k):
-		rateConstant = []
+		rateConstant = [1.,2.5155/60, self.ParamList[k]]
 		SimpleDsbState.rateConstant = rateConstant
 
 	def _ChangeRateConstComplex(self,k):
-		rateConstant = []
+		rateConstant = [1.,4.2257/60,4.5/60,2.7559/60, self.ParamList[k]]
 		ComplexDsbState.rateConstant = rateConstant
